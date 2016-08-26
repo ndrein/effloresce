075217ee987d347881fe1a886020989ecf490f88
Noah Reinhardt <ndrein@gmail.com>
2016-08-26 17:03:46 -0400

    Fixed bug and wrote more tests

--- tests/scanning/test_logic_dfa.py
+++ tests/scanning/test_logic_dfa.py
@@ -46,3 +46,49 @@ class TestScannerLogicDFA(TestScanner):
 
     def test_iff(self):
         self.compare_tokenized_input('<->', [Token('IFF', '<->')])
+
+    def test_simple_sequence(self):
+        self.compare_tokenized_input('forall' ' forallx' ' \n   forallforallx' '01' '||' '!',
+                                     [Token('FORALL', 'forall'),
+                                      Token('ID', 'forallx'),
+                                      Token('ID', 'forallforallx'),
+                                      Token('NUM', '01'),
+                                      Token('OR', '||'),
+                                      Token('NOT', '!')])
+
+    def test_partial_forall(self):
+        self.compare_tokenized_input('for', [Token('ID', 'for')])
+
+    def test_partial_exists(self):
+        self.compare_tokenized_input('e', [Token('ID', 'e')])
+
+    def test_formula(self):
+        self.compare_tokenized_input('forall x ((exists y (P(y) && Q(x, y))) || !exist(x))',
+                                     [Token('FORALL', 'forall'),
+                                      Token('ID', 'x'),
+                                      Token('LPAREN', '('),
+                                      Token('LPAREN', '('),
+                                      Token('EXISTS', 'exists'),
+                                      Token('ID', 'y'),
+                                      Token('LPAREN', '('),
+                                      Token('ID', 'P'),
+                                      Token('LPAREN', '('),
+                                      Token('ID', 'y'),
+                                      Token('RPAREN', ')'),
+                                      Token('AND', '&&'),
+                                      Token('ID', 'Q'),
+                                      Token('LPAREN', '('),
+                                      Token('ID', 'x'),
+                                      Token('COMMA', ','),
+                                      Token('ID', 'y'),
+                                      Token('RPAREN', ')'),
+                                      Token('RPAREN', ')'),
+                                      Token('RPAREN', ')'),
+                                      Token('OR', '||'),
+                                      Token('NOT', '!'),
+                                      Token('ID', 'exist'),
+                                      Token('LPAREN', '('),
+                                      Token('ID', 'x'),
+                                      Token('RPAREN', ')'),
+                                      Token('RPAREN', ')')])
+
