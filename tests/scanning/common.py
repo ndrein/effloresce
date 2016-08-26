9b3bfbba95ec732cb14984d9c7035a36f1b19e73
Noah Reinhardt <ndrein@gmail.com>
2016-08-26 16:45:50 -0400

    Abstracted TestScanner into an abstract class and wrote more test cases

--- tests/scanning/common.py
+++ tests/scanning/common.py
@@ -1,8 +1,23 @@
-def compare_token_sequences(computed_tokens, correct_tokens):
-    """
-    Compare 2 sequences of tokens and raise an Exception if they are not equal
-    """
-    assert(len(computed_tokens) == len(correct_tokens))
-    for correct_token, computed_token in zip(correct_tokens, computed_tokens):
-        assert(correct_token.type == computed_token.type)
-        assert(correct_token.lexeme == computed_token.lexeme)
+"""Common code that can be used in multiple test case for the Scanner"""
+import unittest
+
+from .src import Scanner, logic_dfa
+
+
+class TestScanner(unittest.TestCase):
+    def setUp(self):
+        self.scanner = Scanner(logic_dfa)
+
+    def compare_tokenized_input(self, input, correct_tokens):
+        """
+        Tokenize the input into a sequence of tokens.
+        Compare the computed tokens to the correc_tokens.
+        :param input: sequence of symbols
+        :param correct_tokens: sequence of Tokens
+        :raises: Exception if input is incorrectly tokenized
+        """
+        computed_tokens = self.scanner.tokenize(input)
+        assert(len(computed_tokens) == len(correct_tokens))
+        for correct_token, computed_token in zip(correct_tokens, computed_tokens):
+            assert(correct_token.type == computed_token.type)
+            assert(correct_token.lexeme == computed_token.lexeme)
--- tests/scanning/test_logic_dfa.py
+++ tests/scanning/test_logic_dfa.py
@@ -3,27 +3,46 @@ import unittest
 
 from .src import Scanner, Token, logic_dfa
 
-from .common import compare_token_sequences
+from .common import TestScanner
 
-class ScannerTest(unittest.TestCase):
-    def setUp(self):
-        self.scanner = Scanner(logic_dfa)
 
+class TestScannerLogicDFA(TestScanner):
     def test_zero(self):
-        computed_tokens = self.scanner.tokenize('0')
-        correct_tokens = [Token('NUM', '0')]
-        compare_token_sequences(computed_tokens, correct_tokens)
+        self.compare_tokenized_input('0', [Token('NUM', '0')])
 
     def test_id(self):
-        computed_tokens = self.scanner.tokenize('var')
-        correct_tokens = [Token('ID', 'var')]
-        compare_token_sequences(computed_tokens, correct_tokens)
+        self.compare_tokenized_input('var', [Token('ID', 'var')])
 
     def test_forall(self):
-        compare_token_sequences(self.scanner.tokenize('forall'), [Token('FORALL', 'forall')])
+        self.compare_tokenized_input('forall', [Token('FORALL', 'forall')])
 
     def test_exists(self):
-        compare_token_sequences(self.scanner.tokenize('exists'), [Token('EXISTS', 'exists')])
+        self.compare_tokenized_input('exists', [Token('EXISTS', 'exists')])
 
     def test_num(self):
-        compare_token_sequences(self.scanner.tokenize('00173928570'), [Token('NUM', '00173928570')])
+        self.compare_tokenized_input('00173928570', [Token('NUM', '00173928570')])
+
+    def test_and(self):
+        self.compare_tokenized_input('&&', [Token('AND', '&&')])
+
+    def test_or(self):
+        self.compare_tokenized_input('||', [Token('OR', '||')])
+
+    def test_not(self):
+        self.compare_tokenized_input('!', [Token('NOT', '!')])
+
+    def test_comma(self):
+        self.compare_tokenized_input(',', [Token('COMMA', ',')])
+
+    def test_parens(self):
+        self.compare_tokenized_input('())(',
+                                     [Token('LPAREN', '('),
+                                      Token('RPAREN', ')'),
+                                      Token('RPAREN', ')'),
+                                      Token('LPAREN', '(')])
+
+    def test_implies(self):
+        self.compare_tokenized_input('->', [Token('IMPLIES', '->')])
+
+    def test_iff(self):
+        self.compare_tokenized_input('<->', [Token('IFF', '<->')])
