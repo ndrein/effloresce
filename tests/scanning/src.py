46839b6c6dc0358282952690e102fc4ede8788a7
Noah Reinhardt <ndrein@gmail.com>
2016-08-25 21:28:19 -0400

    Restructuring files

--- tests/scanning/src.py
+++ tests/scanning/src.py
@@ -2,6 +2,5 @@
 Import the source code here, and then all tests will import code from this file
 Update this file when source file structure changes
 """
-
-from effloresce.parsing.parse import Parser
-from effloresce.parsing.dfa import DFA
+from effloresce.syntax.scanning.dfa import DFA
+from effloresce.syntax.scanning.scanner import Scanner
