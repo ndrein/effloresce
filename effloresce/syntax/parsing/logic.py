7cc008d93bbfdc1db5d517e259ab1153bedb5409
Noah Reinhardt <ndrein@gmail.com>
2016-09-06 12:18:20 -0400

    First attempt at CFG for logical formulas

--- effloresce/syntax/parsing/logic.py
+++ effloresce/syntax/parsing/logic.py
@@ -1,1 +1,16 @@
 # TODO: CFG for logical formulas
+formula -> ID LPAREN RPAREN
+formula -> ID LPAREN arglist RPAREN
+formula -> BOOL
+formula -> LPAREN formula AND formula RPAREN
+formula -> LPAREN formula OR formula RPAREN
+formula -> LPAREN formula IMPLIES formula RPAREN
+formula -> LPAREN formula IFF formula RPAREN
+formula -> LPAREN NOT formula RPAREN
+formula -> LPAREN FORALL ID formula RPAREN
+formula -> LPAREN EXISTS ID formula RPAREN
+arglist -> expr
+arglist -> expr COMMA arglist
+expr -> ID
+expr -> ID LPAREN RPAREN
+expr -> ID LPAREN arglist RPAREN
