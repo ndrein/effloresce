97cafde8039592ca35d9c3bd5c054c87f7bb5350
Noah Reinhardt <ndrein@gmail.com>
2019-06-05 14:25:40 -0400

    Rename to _eval and _get_interpretations

--- effloresce/formula.py
+++ effloresce/formula.py
@@ -18,20 +18,20 @@ class Formula:
             raise InvalidFormula
 
     @classmethod
-    def _evaluate(cls, tree: Tree, interpretation: Dict) -> bool:
+    def _eval(cls, tree: Tree, interpretation: Dict) -> bool:
         """Recurses on smaller trees"""
 
         def _make_nullary_op(bin_op: Callable[[Any, Any], bool]) -> Callable[[], bool]:
             return lambda: bin_op(
-                cls._evaluate(tree.children[0], interpretation),
-                cls._evaluate(tree.children[1], interpretation),
+                cls._eval(tree.children[0], interpretation),
+                cls._eval(tree.children[1], interpretation),
             )
 
         if isinstance(tree, Token):
             return interpretation[tree]
 
         return {
-            "not": lambda: not cls._evaluate(tree.children[0], interpretation),
+            "not": lambda: not cls._eval(tree.children[0], interpretation),
             "or": _make_nullary_op(or_),
             "and": _make_nullary_op(and_),
             "implies": _make_nullary_op(lambda p, q: not p or q),
@@ -39,7 +39,7 @@ class Formula:
         }[tree.data]()
 
     def evaluate(self, interpretation: Dict[str, bool]) -> bool:
-        return self._evaluate(self.tree, interpretation)
+        return self._eval(self.tree, interpretation)
 
     def _get_literals(self) -> List:
         """Return all literals in self.tree"""
@@ -53,7 +53,7 @@ class Formula:
         return list(get_literals(self.tree))
 
     @staticmethod
-    def _get_all_interpretations(literals: List) -> Iterable[Dict]:
+    def _get_interpretations(literals: List) -> Iterable[Dict]:
         """Generate all possible interpretations"""
         for booleans in product(*[{False, True}] * len(literals)):
             yield dict(zip(literals, booleans))
@@ -62,9 +62,9 @@ class Formula:
         """Determines whether formula is true for every interpretation that satisfies self"""
         return all(
             (
-                self._evaluate(formula.tree, interp)
-                for interp in self._get_all_interpretations(self._get_literals())
-                if self._evaluate(self.tree, interp)
+                self._eval(formula.tree, interp)
+                for interp in self._get_interpretations(self._get_literals())
+                if self._eval(self.tree, interp)
             )
         )
 
