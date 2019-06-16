1cc75550457a7d2b5ba21b7bbee9f28de9df11a2
Noah Reinhardt <ndrein@gmail.com>
2019-06-15 21:47:43 -0400

    Document proof checker and grammar

--- effloresce/proof_checker.py
+++ effloresce/proof_checker.py
@@ -6,13 +6,20 @@ from lark import Tree
 from effloresce.formula import Formula
 
 
-def check(assumptions: List[Formula] = None, inferences: List[Formula] = None):
+def check(assumptions: List[Formula] = None, inferences: List[Formula] = None) -> bool:
+    """
+    Check if a proof is valid by determining whether
+    all of the inferences follow from each other or the given assumptions
+
+    :param assumptions: collection of Formulas.  Must contain only nand connectives.
+    :param inferences: Formulas derived from previous inferences or the assumptions.  Must contain only nand connectives.
+    """
     assumptions = assumptions or []
     inferences = inferences or []
     return all((_is_valid_inference(inf, assumptions) for inf in inferences))
 
 
-def _is_valid_inference(inf: Formula, assumptions):
+def _is_valid_inference(inf: Formula, assumptions: List[Formula]):
     return any(
         isinstance(a.tree, Tree) and inf.tree == a.tree.children[1].children[1]
         for a in assumptions
